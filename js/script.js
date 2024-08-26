b91f6c16858446a011304f7f2deef68a7c8ae9c7
bruno.duarte <bruno.duarte@zaffari.com.br>
2024-08-26 13:42:53 -0300

    Refatorando fetch-bitcoin

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch-animais.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import fetchAnimais from './modules/fetch-animais.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -18,22 +18,23 @@ const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
 
-const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section')
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
 
 
-const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]')
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
 
 
-const tooltip = new Tooltip('[data-tooltip]')
+const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
 
 
+fetchAnimais('./animaisapi.json', '.numeros-grid');
+
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco')
 
 initAnimacaoScroll();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
-initFetchBitcoin();
