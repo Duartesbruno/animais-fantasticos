20bbba9349408e6ca123b1fcc8772a4ec2d404e1
bruno.duarte <bruno.duarte@zaffari.com.br>
2024-09-09 16:02:28 -0300

    Refatorando o funcionamento

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
 import MenuMobile from './modules/menu-mobile.js';
-import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
+import Funcionamento from './modules/funcionamento.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -46,5 +46,6 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
-
-initFuncionamento();
+const funcionamento = new Funcionamento('[data-semana]', 'aberto')
+funcionamento.init();
+console.log(funcionamento);
